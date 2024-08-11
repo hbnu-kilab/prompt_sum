92b1e1245d54df238f25f33e7ff8b5c81c43e313
parkce <parkce3@gmail.com>
2024-08-11 17:55:17 +0900

    update

--- do_ex.py
+++ do_ex.py
@@ -1,13 +1,11 @@
 from pathlib import Path
-from tqdm import tqdm
 from loader import DataLoader, JsonLoader, JsonInDirLoader, SummaryLoader, SummarySBSCLoader, SummarySDSCLoader, SummaryAIHubNewsLoader
 from promptor import Promptor, Gemma2Promptor
+from promptor.mk_instruction import mk_inst_for_summary
 
 from eval import eval
-from mk_instruction import *
 
 ROOT_DIR = "/kilab/data/"
-
 
 data_type = "news"
 
@@ -28,45 +26,7 @@ elif data_type == "news":
     src_lst, sum_lst = sum_loader.load(json_obj)
 
 promptor = Promptor(Gemma2Promptor)
-'''
-# model_id = "rtzr/ko-gemma-2-9b-it"
-# pipeline = transformers.pipeline(
-#     "text-generation",
-#     model=model_id,
-#     model_kwargs={"torch_dtype": torch.bfloat16},
-#     device_map="auto",
-# )
 
-# pipeline.model.eval()
-
-
-# def do_llm(instruction):
-#     messages = [
-#         {"role": "user", "content": f"{instruction}"}
-#     ]
-
-#     prompt = pipeline.tokenizer.apply_chat_template(
-#         messages, 
-#         tokenize=False, 
-#         add_generation_prompt=True
-#     )
-
-#     terminators = [
-#         pipeline.tokenizer.eos_token_id,
-#         pipeline.tokenizer.convert_tokens_to_ids("<end_of_turn>")
-#     ]
-
-#     outputs = pipeline(
-#         prompt,
-#         max_new_tokens=2048,
-#         eos_token_id=terminators,
-#         do_sample=True,
-#         temperature=0.6,
-#         top_p=0.9,
-#     )
-
-#     return outputs, outputs[0]["generated_text"][len(prompt):]
-'''
 output_sum_lst = []
 for i, (src, sum) in enumerate(zip(src_lst, sum_lst)):
     prev_gold_sum = sum_lst[i-1]
