e43937782b25963468085bd8eb01796f1eaf2256
parkce <parkce3@gmail.com>
2024-10-17 12:41:29 +0900

    update

--- promptor/mk_instruction.py
+++ promptor/mk_instruction.py
@@ -102,11 +102,12 @@ def mk_inst_exsum_wo_noise(sent, sum_ids):
     return f"""주어진 입력 문장을 아래 <order>에 따라 결과를 생성해줘.
 <order>:
 1. 입력 문장은 [num] sentence 가 연결된 meeting dialogue 입니다.
-2. 전체 회의에서 추출된 요약인 extractive summary의 id 리스트가 주워집니다.
-3. 전체 대화를 파악하여 입력된 extractive summary과 관련있는 문장들 선별해주세요.
+2. 전체 회의에서 추출된 요약인 summary에 해당하는 id 리스트 [SUM_IDS] 가 주워집니다.
+3. 전체 대화를 파악하여 입력된 summary에 도움이 되는 문장들 선별해주세요.
 4. 인삿말이나 추임새, extractive summary와 관련 없는 문장들은 제외합니다.
-5. 선별된 문장의 id를 출력하면 되며, 출력 형식은 다음 조건을 따르면 됩니다.
-6. 출력은 선별한 문장의 id를 다음과 같이 0, 1, 2, ... 나열되는 형식으로 결과 id 리스트: 뒤에 출력하세요.
+5. [SUM_IDS]를 제외한 선별된 문장의 id들만 출력하면 되며, 출력 형식은 다음 조건을 따르면 됩니다.
+6. 선별한 문장의 id를 다음과 같이 0, 1, 2, ... 나열되는 형식으로 결과 id 리스트: 뒤에 출력하세요.
+
 
 <format>:
 Extractive summary ids: {sum_ids}
