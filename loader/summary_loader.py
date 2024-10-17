0c35a4283dab2a714edd797be2411d294fee595d
parkce <parkce3@gmail.com>
2024-10-17 12:05:22 +0900

    update

--- loader/summary_loader.py
+++ loader/summary_loader.py
@@ -6,7 +6,7 @@ from .data_loader_interface import DataLoaderInterface
 class SummaryLoader(DataLoaderInterface):
     def __init__(self, 
                  file_system: DataLoaderInterface):
-        self.file_system = file_system
+        self.file_system = file_system()
     
     def load(self, json_lst, **kwargs):
         return self.file_system.load(json_lst, **kwargs)
