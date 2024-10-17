e868bebb5b2b037b0576bd8eb81cda4286b2724d
parkce <parkce3@gmail.com>
2024-10-18 00:07:58 +0900

    update

--- do_eval_meetsum.py
+++ do_eval_meetsum.py
@@ -39,6 +39,7 @@ def load_mode(args):
 
 
 def do_eval_meeting_summary(args, promptor, json_lst):
+    aug_ids_lst, ex_ids_lst = [], []
     for i, ori in tqdm(enumerate(json_lst), total=len(json_lst)):
         # make dialogue with sent_id
         dialog_str = ' '.join([f'[{k}] {v.get("sentence")}' for k, v in json_lst['dialogue'].items()])
@@ -55,7 +56,8 @@ def do_eval_meeting_summary(args, promptor, json_lst):
         tmp_aug = aug_data.split(': ')[-1].strip()
         try:
             if tmp_aug[-1] == '.': tmp_aug = tmp_aug[:-1]
-            
+            if "[결과 id 리스트]:" in tmp_aug: tmp_aug = tmp_aug.split("[결과 id 리스트]: ")[-1]
+
             if tmp_aug[0] == '[' and tmp_aug[-1] != ']': tmp_aug += ']'
             elif tmp_aug[0] != '[' and tmp_aug[-1] == ']': tmp_aug = '[' + tmp_aug
 
@@ -67,11 +69,17 @@ def do_eval_meeting_summary(args, promptor, json_lst):
         if 0 in aug_ids:
             del aug_ids[aug_ids.index(0)]
 
-def eval(output_doc_ids, oracle_doc_ids):
+        aug_ids_lst.append(aug_ids)
+        ex_ids_lst.append(ex_ids)
+
+    ex_eval(aug_ids, ex_ids)
+
+
+def ex_eval(output_doc_ids, oracle_doc_ids):
     # accuracy
-    indexes = torch.tensor(output_doc_ids)
-    target = torch.tensor(oracle_doc_ids)
-    # target = [torch.tensor(o_el) for o_el in oracle_doc_ids]
+    # indexes = torch.tensor(output_doc_ids)
+    indexes = [torch.tensor(o_el) for o_el in output_doc_ids]
+    target = [torch.tensor(o_el) for o_el in oracle_doc_ids]
     
     # macro f1
     hits = [torch.isin(idx, tgt).sum().item() for idx, tgt in zip(indexes, target)]
@@ -92,17 +100,17 @@ def eval(output_doc_ids, oracle_doc_ids):
     avg_score = c_cnt/o_cnt * 100
     
     # torchmetric
-    preds = torch.cat(output_doc_scores).to("cpu")
-    target_mask = torch.cat([torch.isin(idx, gold).long().unsqueeze(0) for idx, gold in zip(indexes, target)])
-    rp = self.rp(preds, target_mask, indexes=indexes)
-    r2 = self.r2(preds, target_mask, indexes=indexes)
-    hr = self.hr(preds, target_mask, indexes=indexes)
-    map = self.map(preds, target_mask, indexes=indexes)
-    mrr = self.mrr(preds, target_mask, indexes=indexes)
-    ndcg = self.ndcg(preds, target_mask, indexes=indexes)
+    # preds = torch.cat(output_doc_scores).to("cpu")
+    # target_mask = torch.cat([torch.isin(idx, gold).long().unsqueeze(0) for idx, gold in zip(indexes, target)])
+    # rp = self.rp(preds, target_mask, indexes=indexes)
+    # r2 = self.r2(preds, target_mask, indexes=indexes)
+    # hr = self.hr(preds, target_mask, indexes=indexes)
+    # map = self.map(preds, target_mask, indexes=indexes)
+    # mrr = self.mrr(preds, target_mask, indexes=indexes)
+    # ndcg = self.ndcg(preds, target_mask, indexes=indexes)
 
     print(f"Score: [ACC] {avg_score:.2f}, [PREC] {pre:.2f}, [REC] {rec:.2f}, [F1] {f1:.2f}")
-    print(f"Retrieval Score: [Hits] {hr:.4f}, [RPEC] {rp:.4f}, [RREC] {r2:.4f}, [MAP] {map:.4f}, [MRR] {mrr:.4f}, [NDGC] {ndcg:.4f}")
+    # print(f"Retrieval Score: [Hits] {hr:.4f}, [RPEC] {rp:.4f}, [RREC] {r2:.4f}, [MAP] {map:.4f}, [MRR] {mrr:.4f}, [NDGC] {ndcg:.4f}")
     print(f"output doc ids: {output_doc_ids[0]}")
     print(f"oracle doc ids: {oracle_doc_ids[0]}")
     print("-------------------------")
