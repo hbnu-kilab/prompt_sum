d50169c297ca35a300020305e076f45232c4ee05
parkce <parkce3@gmail.com>
2024-10-17 12:17:28 +0900

    update

--- promptor/mk_instruction.py
+++ promptor/mk_instruction.py
@@ -103,8 +103,8 @@ def mk_inst_exsum_wo_noise(sent, sum_ids):
 <order>:
 1. 주어진 입력은 [num] sentence 가 연결된 meeting dialogue 입니다.
 2. 전체 회의에서 {sum_ids}의 문장이 extractive summary 입니다.
-3. 전체 dialogue의 문장들 중에서 extractive summary와 관련있는 문장들만 추리세요.
-4. 출력은 입력 구조와 동일하게 [num] sentence 가 연결된 형태로 생성합니다.
+3. 전체 dialogue의 문장들 중에서 extractive summary와 관련있는 문장들만 추려서 문장의 id를 출력하세요.
+4. 출력은 리스트 형식으로 [0, 1, 2, ...] 와 같이 출력하세요.
 5. 이때 관련있는 문장들과 extractive summary를 포함하여 문장 번호 순서대로 출력하세요.
 
 <format>:
